faab048b51c8173afcb8d35f021792247132343e
ilyakozhevn <kozhevilya@gmail.com>
2022-07-20 18:03:59 +0300

    numberOfSections deleted

--- ContactListApp/ViewControllers/PersonsListViewController.swift
+++ ContactListApp/ViewControllers/PersonsListViewController.swift
@@ -13,10 +13,6 @@ class PersonsListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return persons.count 
     }
